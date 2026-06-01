a985f796e28a73e4b6adae6b38a0f4be54669e9e
Swagi-REDACTED <111149273+Swagi-REDACTED@users.noreply.github.com>
2026-06-01 02:50:12 -0400

    Delete src/main directory

--- src/main/java/com/blakebr0/ironjetpacks/TagTest2.java
+++
@@ -1,8 +0,0 @@
-package com.blakebr0.ironjetpacks;
-public class TagTest2 {
-    public static void main(String[] args) {
-        String s = "tag:c:ingots/copper";
-        String[] parts = s.split(":");
-        System.out.println(java.util.Arrays.toString(parts));
-    }
-}
