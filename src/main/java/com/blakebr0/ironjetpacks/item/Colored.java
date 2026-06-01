0ededfc148702f033182dfb6c0bc25f0e9d02a06
Swagi-REDACTED <111149273+Swagi-REDACTED@users.noreply.github.com>
2026-05-31 21:55:10 -0400

    Delete src/main/java/com/blakebr0/ironjetpacks directory

--- src/main/java/com/blakebr0/ironjetpacks/item/Colored.java
+++
@@ -1,9 +0,0 @@
-package com.blakebr0.ironjetpacks.item;
-
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-
-public interface Colored {
-    @Environment(EnvType.CLIENT)
-    int getColorTint(int tintIndex);
-}
--- src/main/java/com/blakebr0/ironjetpacks/item/Enableable.java
+++
@@ -1,5 +0,0 @@
-package com.blakebr0.ironjetpacks.item;
-
-public interface Enableable {
-    boolean isEnabled();
-}
